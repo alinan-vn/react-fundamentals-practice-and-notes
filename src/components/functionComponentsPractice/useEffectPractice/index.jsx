21d091ec63487fdc050a84e949ac799803086c28
Alejandro Linan <jandrolinan@gmail.com>
2021-05-11 12:07:33 -0500

    added useEffect example

--- src/components/functionComponentsPractice/useEffectPractice/index.jsx
+++ src/components/functionComponentsPractice/useEffectPractice/index.jsx
@@ -8,9 +8,21 @@ function UseEffectPractice(){
         document.title = `You clicked ${count} times`
     })
 
+    const [updateCount, setUpdateCount] = useState(0);
+    const [customText, setCustomText] = useState('');
+
+    function updateCustomText(){
+        let customTextInput = document.querySelector('#customText');
+        setCustomText(customTextInput.value);
+    }
+
+    useEffect(() => {
+        setUpdateCount(updateCount + 1);
+    })
+
     return(
         <div className='useEffect'>
-            
+
             <h1>'useEffect practice examples'</h1>
 
             <Card className='useEffect-card' body inverse style={{ backgroundColor: '#999', borderColor: '#555' }}>
@@ -18,26 +30,18 @@ function UseEffectPractice(){
                 <CardText>You clicked { count } times!</CardText>
                 <Button onClick={() => setCount(count + 1)}>Click Me!</Button>
             </Card><br />
+
+            <Card className='useEffect-card' body inverse style={{ backgroundColor: '#999', borderColor: '#555' }}>
+                <CardTitle tag="h5">"useEffect" Practice example</CardTitle>
+                <CardText>No. of times values were updated: {updateCount}</CardText>
+                <CardText>***NOTE that the value is updated automatically in the useEffect Hook</CardText>
+                <CardText>This is your custom text: {customText}</CardText>
+                <input id='customText' placeholder='type some stuff!' />
+                <Button onClick={updateCustomText}>Update the text</Button>
+            </Card><br />
+
         </div>
     )
 }
 
 export default UseEffectPractice;
-
-// function Example() {
-//     const [count, setCount] = useState(0);
-  
-//     // Similar to componentDidMount and componentDidUpdate:
-//     useEffect(() => {
-//       // Update the document title using the browser API
-//       document.title = `You clicked ${count} times`;
-//     });
-  
-//     return (
-//       <div>
-//         <p>You clicked {count} times</p>
-//         <button onClick={() => setCount(count + 1)}>
-//           Click me
-//         </button>
-//       </div>
-//     );
